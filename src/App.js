61a90fa9eaeb8a8e4ea9eafabe68826cc00fa53a
Dorabang <psa1195@naver.com>
2023-06-29 12:28:26 +0900

    design: css 초기화

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import { styled } from 'styled-components';
+import Nav from './components/Nav';
+import Banner from './components/Banner';
 import './App.css';
+import Category from './components/Category';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Nav />
+      <Banner />
+      <Category />
+    </Container>
   );
-}
+};
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &:after {
+    background: url('images/home-background.png') center center no-repeat fixed;
+    background-size: cover;
+    content: '';
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
 
 export default App;
