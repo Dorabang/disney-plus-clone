2c2cee3971e2882f082553974be86d60d4d76e35
Dorabang <psa1195@naver.com>
2023-06-29 12:26:59 +0900

    feat: 카테고리 정보 상수 작성

---
+++ src/components/Category/Constant.js
@@ -0,0 +1,34 @@
+const CategoryList = [
+  {
+    id: 0,
+    image: '/images/viewers-disney.png',
+    alt: 'disney logo',
+    video: '/videos/disney.mp4',
+  },
+  {
+    id: 1,
+    image: '/images/viewers-pixar.png',
+    alt: 'disney logo',
+    video: '/videos/pixar.mp4',
+  },
+  {
+    id: 2,
+    image: '/images/viewers-marvel.png',
+    alt: 'disney logo',
+    video: '/videos/marvel.mp4',
+  },
+  {
+    id: 3,
+    image: '/images/viewers-starwars.png',
+    alt: 'disney logo',
+    video: '/videos/star-wars.mp4',
+  },
+  {
+    id: 4,
+    image: '/images/viewers-national.png',
+    alt: 'disney logo',
+    video: '/videos/national-geographic.mp4',
+  },
+];
+
+export default CategoryList;
