aaca2191701a67f53e9a02ee8a719438e85abfa4
Dorabang <psa1195@naver.com>
2023-07-01 12:58:28 +0900

    feat: 네비게이션 바에 로그인 버튼 및 검색창 추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,33 @@
 import { useState, useEffect } from 'react';
-import { NavWrapper, Logo } from './NavStyled';
+import { NavWrapper, Logo, Login, Input } from './NavStyled';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Nav = () => {
   const [show, setShow] = useState('false');
+  const { pathname } = useLocation();
+  const [searchValue, setSearchValue] = useState('');
+  const Navigate = useNavigate();
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
-      if (window.scrollY > 50) {
-        setShow('true');
-      } else {
-        setShow('false');
-      }
-    });
+    window.addEventListener('scroll', handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', () => {});
+      window.removeEventListener('scroll', handleScroll);
     };
   }, []);
+
+  const handleScroll = () => {
+    if (window.scrollY > 50) {
+      setShow('true');
+    } else {
+      setShow('false');
+    }
+  };
+
+  const handleChange = (e) => {
+    setSearchValue(e.target.value);
+    Navigate(`/search?q=${e.target.value}`);
+  };
 
   return (
     <NavWrapper show={show}>
@@ -27,6 +38,18 @@ const Nav = () => {
           onClick={() => (window.location.href = '/')}
         />
       </Logo>
+
+      {pathname === '/' ? (
+        <Login>Log-in</Login>
+      ) : (
+        <Input
+          value={searchValue}
+          onChange={handleChange}
+          className='nav_input'
+          type='text'
+          placeholder='Search'
+        />
+      )}
     </NavWrapper>
   );
 };
--- src/components/NavStyled.js
+++ src/components/NavStyled.js
@@ -25,9 +25,37 @@ export const Logo = styled.a`
   max-height: 70px;
   font-size: 0;
   display: inline-block;
+  cursor: pointer;
 
   img {
     display: block;
     width: 100%;
   }
+`;
+
+export const Login = styled.a`
+  background: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  letter-spacing: 1px;
+  border: 1px solid #f9f9f9;
+  transition: all 0.2s ease 0s;
+  border-radius: 5px;
+  font-size: 15px;
+  cursor: pointer;
+
+  &:hover {
+    background: #f9f9f9;
+    color: #000;
+  }
+`;
+
+export const Input = styled.input`
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  background: rgba(0, 0, 0, 0.582);
+  border-radius: 5px;
+  color: #fff;
+  padding: 5px;
+  border: none;
 `;
