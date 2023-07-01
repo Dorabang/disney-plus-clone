155b2c6d4c0b5731d103b388f41af47d21b9969d
Dorabang <psa1195@naver.com>
2023-07-01 13:01:15 +0900

    chore: 앱 라우팅으로 메인페이지 따로 생성

---
+++ src/pages/MainPage/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { styled } from 'styled-components';
+import Nav from '../../components/Nav';
+import Banner from '../../components/Banner';
+import Category from '../../components/Category';
+import Row from '../../components/Row';
+import { RowList } from '../../components/Row/Constant';
+import '../../App.css';
+
+const MainPage = () => {
+  return (
+    <Container>
+      <Nav />
+      <Banner />
+      <Category />
+      {RowList.map((row) => (
+        <div key={row.key}>
+          <Row id={row.id} title={row.title} fetchUrl={row.fetchUrl} />
+        </div>
+      ))}
+    </Container>
+  );
+};
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+
+  &:after {
+    background: url('images/home-background.png') center center no-repeat fixed;
+    background-size: cover;
+    content: '';
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
+
+export default MainPage;
