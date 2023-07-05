8be83d3c1b35f72b54852af6a92c98c04053f8d1
Dorabang <psa1195@naver.com>
2023-07-05 13:39:08 +0900

    chore: .env 내에 작성한 movie api key 수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'https://api.themoviedb.org/3',
-  params: { api_key: process.env.REACT_APP_API_KEY, language: 'ko-KR' },
+  params: { api_key: process.env.REACT_APP_MOVIE_API_KEY, language: 'ko-KR' },
 });
 export default instance;
